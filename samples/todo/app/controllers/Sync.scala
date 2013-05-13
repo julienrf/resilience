8d2859530a17eb81d302998b775168549e58aade
Julien Richard-Foy <julien@richard-foy.fr>
2013-05-13 17:44:48 +0200

    componentization

--- samples/todo/app/controllers/Sync.scala
+++ samples/todo/app/controllers/Sync.scala
@@ -8,34 +8,39 @@ import scala.concurrent.Future
 import akka.util.Timeout
 import java.util.concurrent.TimeUnit
 
-// TODO Generalize over the events domain and the state updates
-class Sync extends Actor {
+trait Sync {
 
-  import Sync._
+  // Domain events
+  type Events
+  // Domain interpretation of events
+  def interprete(events: Events)
 
-  val (notifications, channel) = Concurrent.broadcast[Seq[Event]]
+  class SyncActor extends Actor {
 
-  // A batch of events has to be applied
-  def apply(events: Seq[Event]) {
-    // Apply each event (TODO handle failure)
-    events.foreach(State.apply)
-    // Notify each client of the applied actions
-    channel.push(events)
-  }
+    val (notifications, channel) = Concurrent.broadcast[Events]
 
-  def receive = {
-    // A new client joins
-    case Join() =>
-      // We will interprete his actions using this iteratee
-      val interpreter = Iteratee.foreach[Seq[Event]] { apply(_) }
-      // Send back the interpreter and the event notification stream
-      sender ! (interpreter, notifications)
+    // A batch of events has to be applied
+    def apply(events: Events) {
+      interprete(events)
+      // Notify each client of the applied actions
+      channel.push(events)
+    }
+
+    def receive = {
+      // A new client joins
+      case Sync.Join() =>
+        // We will interprete his actions using this iteratee
+        val interpreter = Iteratee.foreach[Events] { apply(_) }
+        // Send back the interpreter and the event notification stream
+        sender ! (interpreter, notifications)
+
+    }
 
   }
 
 }
 
-object Sync {
+object Sync extends Sync {
 
   case class Join()
 
@@ -43,13 +48,18 @@ object Sync {
   import play.api.Play.current
   import akka.pattern.ask
 
-  private val sync = Akka.system.actorOf(Props[Sync])
+  private val sync = Akka.system.actorOf(Props(new SyncActor))
 
   implicit val timeout = Timeout(1, TimeUnit.SECONDS)
 
-  type Events = Seq[Event]
-
   def join: Future[(Iteratee[Events, _], Enumerator[Events])] =
     (sync ? Join()).mapTo[(Iteratee[Events, _], Enumerator[Events])]
+
+  type Events = Seq[Event]
+
+  def interprete(events: Events) {
+    // Apply each event (TODO handle failure)
+    events.foreach(State.apply)
+  }
 
 }
