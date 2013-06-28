892e6133fbd4db31d1651377b895ec4db388208a
Julien Richard-Foy <julien@richard-foy.fr>
2013-06-28 09:34:19 +0200

    Fix logging

--- samples/todo/app/business/events.scala
+++ samples/todo/app/business/events.scala
@@ -1,6 +1,6 @@
 package business
 
-sealed trait Event
+sealed trait Event extends julienrf.resilience.EventLike
 case class Added(id: String, itemId: String, content: String, done: Boolean) extends Event
 case class Removed(id: String, itemId: String) extends Event
 case class Toggled(id: String, itemId: String) extends Event
--- samples/todo/app/julienrf/resilience/Sync.scala
+++ samples/todo/app/julienrf/resilience/Sync.scala
@@ -3,10 +3,16 @@ package julienrf.resilience
 import play.api.libs.iteratee.{Iteratee, Concurrent}
 import play.api.libs.json.{JsObject, Writes, Reads, Json}
 import play.api.Logger
-import play.modules.reactivemongo.ReactiveMongoPlugin
+import reactivemongo.core.commands.{Update, FindAndModify}
+import reactivemongo.bson.BSONDocumentWriter
 import play.modules.reactivemongo.json.collection.JSONCollection
+import play.modules.reactivemongo.json.ImplicitBSONHandlers._
 import scala.concurrent.{ExecutionContext, Future}
 import scala.concurrent.stm.Ref
+
+trait EventLike {
+  def id: String
+}
 
 trait Sync {
 
@@ -15,7 +21,7 @@ trait Sync {
   trait Sync {
 
     // Domain events
-    type Event
+    type Event <: EventLike
     // Domain interpretation of events (FIXME Make it asynchronous? TODO Handle failure)
     def interprete(event: Event): Unit
 
@@ -28,19 +34,32 @@ trait Sync {
     private val (_notifications, channel) = Concurrent.broadcast[Seq[Event]]
     private val time = Ref(0)
 
-    // A batch of events has to be applied
-    def apply(events: Seq[Event]): Unit = {
-      for (event <- events) {
-        // Apply the event to the application’s state
-        interprete(event)
-        // Log the event in the journal
-        journalCollection
-          .save(Json.obj("time" -> time.single.getAndTransform(_ + 1), "event" -> Json.toJson(event)))
-          .filter(_.ok)
-          .onFailure { case error => Logger.debug(s"There was a problem: $error") }
-      }
-      // Notify each client of the applied actions
-      channel.push(events)
+    // Applies a batch of events
+    private def apply(events: Seq[Event]): Unit = events match {
+      case event +: events =>
+        Logger.debug(s"""findAnModify({ "query": { event.id: "${event.id}" }, "update": {${Json.toJson(event)} })""")
+        journalCollection.db.command(FindAndModify(
+          journalCollection.name,
+          query = implicitly[BSONDocumentWriter[JsObject]].write(Json.obj("event.id" -> event.id)),
+          modify = Update(implicitly[BSONDocumentWriter[JsObject]].write(Json.obj("time" -> time.single.getAndTransform(_ + 1), "event" -> Json.toJson(event))), fetchNewObject = false),
+          upsert = true
+        )).map { maybeOldEntry =>
+            maybeOldEntry match {
+              case Some(oldEntry) =>
+                if (oldEntry.isEmpty) {
+                  // Apply the event to the application’s state
+                  interprete(event)
+                  // Notify each client of the applied actions
+                  channel.push(Seq(event))
+                }
+              case None => Logger.debug("Problem?")
+            }
+          }
+          .onFailure {
+            case error => Logger.debug(s"There was a problem: $error")
+          }
+        apply(events)
+      case _ =>
     }
 
     // Recover state
@@ -48,8 +67,12 @@ trait Sync {
       for (eventsList <- journalCollection.find(Json.obj()).sort(Json.obj("time" -> 1)).cursor[JsObject].toList) yield {
         for {
           entry <- eventsList
+          last <- eventsList.map(entry => (entry \ "time").asOpt[Int]).max
           event <- (entry \ "event").validate[Event]
-        } interprete(event)
+        } {
+          interprete(event)
+          time.single.set(last)
+        }
       }
 
     // Output stream of events
